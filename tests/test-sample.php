51d0c0aeb3f2b9901ac6dbf841732472786285dc
Milan <milan@srpski.biz>
2016-12-09 18:23:06 +0000

    New tests.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -15,6 +15,21 @@ class SampleTest extends WP_UnitTestCase {
 	 */
 	function test_sample() {
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		//$this->assertTrue( true );
+	}
+
+	function test_cleaning() {
+		$set = set_transient( 'row', 'that', 5 );
+		$this->assertTrue( $set );
+
+		$get = get_transient( 'row' );
+		$this->assertEquals( $get, 'that' );
+
+		sleep(20);
+
+		Clean_Expired_Transients::clean();
+
+		$fresh = get_transient( 'row' );
+		$this->assertFalse( $fresh );
 	}
 }
